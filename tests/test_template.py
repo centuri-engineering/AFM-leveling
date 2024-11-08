f58cc6b19f5632b2df54c78629d557563ddd11fa
LE D.Q. Thang <ldqthang@gmail.com>
2024-11-08 12:18:51 +0100

    Add template for running your own codes

---
+++ tests/test_template.py
@@ -0,0 +1,53 @@
+import context
+
+####################
+# Import modules here 
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    ########### Put your codes here ###################
+    
+    #########################################
+    ### Step 1.1 - Load AFM jpk text file ###
+
+
+    
+    ##########################################
+    ### Step 1.2 - Load AFM image TIF file ###
+
+
+    
+    ##########################################################################
+    ### Step 2 - Do data pre-processing such as scan line artefact removal ###
+    
+    
+    
+    ##########################################################
+    ### Step 3 - Segmentation of foreground and background ###
+    
+    
+    
+    ##############################
+    ### Step 4 - Data leveling ###
+    
+    
+    
+    ########################################################################
+    ### Step 5 (Optional) Do data post-processing such as removing scars ###
+    
+    
+    
+    ############################################################################################
+    ### Step 6 (Optional) Save all results and figures in a subfolder of the folder "output" ###
+    
